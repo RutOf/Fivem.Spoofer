ad11101f6f0ff134706ef5289009a4bb53401fde
Smith <98755336+JackerSmith@users.noreply.github.com>
2022-10-08 07:11:15 +0700

    Update cleaner.cpp

--- Source C++/cleaner.cpp
+++ Source C++/cleaner.cpp
@@ -5,91 +5,28 @@
 #include "encryption.h"
 #include "WEB.h"
 
+static const char alphanum[] = "0123456789" "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz"; // he has a brain atleast
 
-// Where the magic happens
+int stringLength1 = sizeof(alphanum) - 1;
 
-
-	/* The size of the memory that we will look for the pattern */
-	auto size_to_scan = 4096 * 100;
-	
-
-	/* Now we call the function that will search for the pattern*/
-	DWORD* address = (DWORD*)ExPatternScanByStartAddress(hprocess, main_module_address, size_to_scan, pattern_byte, mask);
-	if (!address) {
-		SetConsoleTextAttribute(console, 0x0c);
-		cout << "Error. Address for hook wasn't found. This may be because you already executed this program or because somehow the bypass is outdate. Trying again anyway..." << endl;
-		Sleep(10000);
-		system("cls");
-		main();
-	}
-	
-	/* As the pattern that I scanned is -6 that the address that I want to modify, i am increasing 6  to this */
-	address = (DWORD*)((DWORD)address + 6); 
-	// cout << "The hook address was found. It is: 0x" << hex << address << endl;
-	
-	/* Now we need to know where the return address should be. Because we will hook the address with a jump */
-	/* The return address is address + 8. This is where the jmp and nops are finisheds */
-	DWORD* return_hook_address = (DWORD*)((DWORD)address + 8);
-
-	/* Now we alloc memory to the target process. We will need this to hook. VirtualAllocEx  will return the address were it was allocated	*/
-	DWORD* alloc_address = (DWORD*) VirtualAllocEx(hprocess, 0, 1024, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-	if (!alloc_address) {
-		SetConsoleTextAttribute(console, 0x0c);
-		cout << "Error while allocating memory..." << endl;
-		Sleep(3000);
-		main();
-	}
-
-
-int choice;
-Encryption encryption = Encryption();
-WEB webb = WEB();
-
-
-int main()
+char genRandom1()
 {
-	webb.CheckVersion("2.0");
-	Spoofing spoofer = Spoofing();
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 12);
-	std::cout << " _______  _______  _______  _______  _______  _______  ______   " << std::endl;
-	std::cout << "|       ||       ||       ||       ||       ||       ||    _ |  " << std::endl;
-	std::cout << "|  _____||    _  ||   _   ||   _   ||    ___||    ___||   | ||  " << std::endl;
-	std::cout << "| |_____ |   |_| ||  | |  ||  | |  ||   |___ |   |___ |   |_||_ " << std::endl;
-	std::cout << "|_____  ||    ___||  |_|  ||  |_|  ||    ___||    ___||    __  |" << std::endl;
-	std::cout << "|_____| ||   |    |       ||       ||   |    |   |___ |   |  | |" << std::endl;
-	std::cout << "|_______||___|    |_______||_______||___|    |_______||___|  |_|" << std::endl;
-	SetConsoleTextAttribute(hConsole, 14);
-	std::cout << encryption.GetCredits().c_str() << std::endl;
-	SetConsoleTextAttribute(hConsole, 15);
-	system("pause");
-	while (true) {
-		system("cls");
-		SetConsoleTextAttribute(hConsole, 12);
-		std::cout << " _______  _______  _______  _______  _______  ___   __    _  _______             " << std::endl;
-		std::cout << "|       ||       ||       ||       ||       ||   | |  |  | ||       |" << std::endl;
-		std::cout << "|  _____||    _  ||   _   ||   _   ||    ___||   | |   |_| ||    ___|  " << std::endl;
-		std::cout << "| |_____ |   |_| ||  | |  ||  | |  ||   |___ |   | |       ||   | __    " << std::endl;
-		std::cout << "|_____  ||    ___||  |_|  ||  |_|  ||    ___||   | |  _    ||   ||  | ___   ___   ___  " << std::endl;
-		std::cout << " _____| ||   |    |       ||       ||   |    |   | | | |   ||   |_| ||   | |   | |   | " << std::endl;
-		std::cout << "|_______||___|    |_______||_______||___|    |___| |_|  |__||_______||___| |___| |___| " << std::endl;
-		SetConsoleTextAttribute(hConsole, 10);
-		std::cout << "\nVersion 2.0\n" << std::endl;
-		std::cout << "\nSelect game that you want to spoof...\n" << std::endl;
-		SetConsoleTextAttribute(hConsole, 15);
-		std::cout << "[1] << \x1B[36mFiveM\033[0m\t\t" << std::endl;
-		SetConsoleTextAttribute(hConsole, 15);
-		std::cout << "[2] << \x1B[36msecond game\033[0m\t\t" << std::endl;
-		SetConsoleTextAttribute(hConsole, 15);
-		std::cin >> choice;
-		if (choice && choice > 0 && choice <= 2) {
-			switch (choice) {
-			case 1: // fivem case
-				spoofer.Initialize();
-				system("pause");
-				break;
-			}
-		}
+
+    return alphanum[rand() % stringLength1];
+}
+
+
+void cleaner::Fnoberz()
+{
+    HWND hwnd = GetConsoleWindow();
+    DWORD style = GetWindowLong(hwnd, GWL_STYLE);
+    style &= ~WS_MAXIMIZEBOX & ~WS_SIZEBOX;
+    SetWindowLong(hwnd, GWL_STYLE, style);
+    GetWindowRect(hwnd, &rc);
+    int xPos = (GetSystemMetrics(SM_CXSCREEN) - rc.right) / 2;
+    int yPos = (GetSystemMetrics(SM_CYSCREEN) - rc.bottom) / 2;
+    SetWindowPos(hwnd, 0, xPos, yPos, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
+}
 	}
 
 }
