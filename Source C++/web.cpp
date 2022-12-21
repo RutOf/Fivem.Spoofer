a1a2e39de1b77e202056a7da1ce2ff81ac1daf1a
Slackes <SarnaxOfficial@protonmail.com>
2022-12-22 02:40:14 +0700

    Update web.cpp

--- Source C++/web.cpp
+++ Source C++/web.cpp
@@ -1,47 +1,55 @@
-#include "WEB.cpp"
+#include <string>
+#include <windows.h>
+#include <wininet.h>
+#include <dxgi1_6.h>
+#include <d3d11_5.h>
 
+#include "WEB.h"
 
-string WEB::replaceAll(string subject, const string& search,
-	const string& replace) {
-	size_t pos = 0;
-	while ((pos = subject.find(search, pos)) != string::npos) {
-		subject.replace(pos, search.length(), replace);
-		pos += replace.length();
-	}
-	return subject;
+std::string WEB::replaceAll(std::string subject, const std::string& search,
+                            const std::string& replace)
+{
+    size_t pos = 0;
+    while ((pos = subject.find(search, pos)) != std::string::npos)
+    {
+        subject.replace(pos, search.length(), replace);
+        pos += replace.length();
+    }
+    return subject;
 }
 
-string WEB::DownloadString(string URL) {
-	HINTERNET interwebs = InternetOpenA("Mozilla/5.0", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, NULL);
-	HINTERNET urlFile;
-	string rtn;
-	if (interwebs) {
-		urlFile = InternetOpenUrlA(interwebs, URL.c_str(), NULL, NULL, NULL, NULL);
-		if (urlFile) {
-			char buffer[2000];
-			DWORD bytesRead;
-			do {
-				InternetReadFile(urlFile, buffer, 2000, &bytesRead);
-				rtn.append(buffer, bytesRead);
-				memset(buffer, 0, 2000);
-			} while (bytesRead);
-			InternetCloseHandle(interwebs);
-			InternetCloseHandle(urlFile);
-			string p = replaceAll(rtn, "|n", "\r\n");
-			return p;
-		}
-	}
-	InternetCloseHandle(interwebs);
-	string p = replaceAll(rtn, "|n", "\r\n");
-	return p;
+std::string WEB::downloadString(const std::string& URL)
+{
+    HINTERNET interwebs = InternetOpenA("Mozilla/5.0", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, NULL);
+    HINTERNET urlFile;
+    std::string rtn;
+    if (interwebs)
+    {
+        urlFile = InternetOpenUrlA(interwebs, URL.c_str(), NULL, NULL, NULL, NULL);
+        if (urlFile)
+        {
+            char buffer[2000];
+            DWORD bytesRead;
+            do
+            {
+                InternetReadFile(urlFile, buffer, 2000, &bytesRead);
+                rtn.append(buffer, bytesRead);
+                memset(buffer, 0, 2000);
+            } while (bytesRead);
+            InternetCloseHandle(urlFile);
+        }
+        InternetCloseHandle(interwebs);
+    }
+    return replaceAll(rtn, "|n", "\r\n");
 }
 
-__declspec(dllexport) HRESULT ResizeHook(IDXGISwapChain* swapChain, UINT bufferCount, UINT width, UINT height, DXGI_FORMAT newFormat, UINT swapChainFlags) {
-	ImGui_ImplDX11_Shutdown();
-	renderTargetView->Release();
-	immediateContext->Release();
-	device->Release();
-	device = nullptr;
+HRESULT WEB::resizeHook(IDXGISwapChain* swapChain, UINT bufferCount, UINT width, UINT height, DXGI_FORMAT newFormat, UINT swapChainFlags)
+{
+    ImGui_ImplDX11_Shutdown();
+    renderTargetView->Release();
+    immediateContext->Release();
+    device->Release();
+    device = nullptr;
 
-	return ResizeOriginal(swapChain, bufferCount, width, height, newFormat, swapChainFlags);
+    return resizeOriginal(swapChain, bufferCount, width, height, newFormat, swapChainFlags);
 }
